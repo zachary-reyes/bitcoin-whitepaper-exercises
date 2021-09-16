c3bc4c8031908eea36f31cdfd081afcaa41d8040
Zachary Reyes <zacharyaustin@outlook.com>
2021-09-16 15:33:58 -0700

    part 1 complete

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,8 +28,29 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line)
+}
+
+// Create a function called createBlock()
+// `index`
+// `prevHash`
+// `data`
+// `timestamp`
+// `hash`
+
+function createBlock(data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data: data,
+		timestamp: Date.now()
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+	return block
+}
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
@@ -37,7 +58,10 @@ Blockchain.blocks.push({
 // **********************************
 
 function blockHash(bl) {
+	// let bl = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
+		// block
 	).digest("hex");
 }
